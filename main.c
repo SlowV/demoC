05871a728c418ace615481244868e3d32e5d1e20
Viet <viettqd00622@fpt.edu.vn>
2017-08-31 20:39:39 +0700

    tru 2 so

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
  main(int argc, char *argv[]) {
-	printf("%i", plus(5, 10));
+	printf("%i", minus(5, 10));
 }
 
-int plus(int a, int b){
-	return a + b;
+int minus(int a, int b){
+	return a - b;
 }
