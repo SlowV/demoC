1b9571667e1dbb23ebc1648df06ce7c5ce688d29
Viet <viettqd00622@fpt.edu.vn>
2017-08-31 19:44:08 +0700

    message

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ main(int argc, char *argv[]) {
+	printf("%i", plus(5, 10));
+}
+
+int plus(int a, int b){
+	return a + b;
+}
