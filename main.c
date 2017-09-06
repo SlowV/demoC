acda906ed0b5dc350bdeb3e4528ad0fd4ae0d486
SlowV <viettqd00622@fpt.edu.vn>
2017-09-06 14:12:03 +0700

    Bài tập 5/9

--- main.c
+++ main.c
@@ -1,25 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char *argv[]) {
-int a, b;
-	printf("nhap gia tri cua a:\n");
-		scanf("%i", &a);
-	printf("nhap gia tri cua b:");
-		scanf("%i", &b);
-	printf("%i\n", plus(a, b));
-	printf("%f\n", divi(a, b));
-	printf("%i\n", minus(a, b));
-	printf("%i", multi(a, b));
-}
-int plus(int a, int b){
-	return a+b;
-}
-int divi(int a, int b){
-	return a/b;
-}
-int minus(int a, int b){
-	return a-b;
-}
-int multi(int a, int b){
-	return a*b;
+int main(){
+	int x;
+	int y;
+	int z;
+	while ("69")
+	{
+		system("cls");
+	    printf("\n       TINH TOAN   ");
+	    printf("\n************************ ");
+	    printf("\n1.Tinh tong 2 so x + y. ");
+	    printf("\n2.Tinh hieu 2 so x - y. ");
+	    printf("\n3.Tinh tich 2 so x * y. ");
+	    printf("\n4.Tinh thuong 2 so x / y.");
+	    printf("\n0.Thoat.");
+		printf("\nVui long nhap lua chon: ");
+	    scanf("%i", &z);
+		switch(z){
+			case 1:	
+				printf("Vui long nhap so X: ");
+	    		scanf("%i", &x);
+	    		printf("Vui long nhap so Y: ");
+	    		scanf("%i", &y);
+				printf("\nKet Qua: %i + %i = %i ", x, y, x + y);
+	   			printf("\n    Thank you  ");
+	   			printf("\n********************");
+	   			system("pause");
+	   			break;
+			case 2:
+				printf("Vui long nhap so X:  ");
+	    		scanf("%i", &x);
+	    		printf("Vui long nhap so Y: ");
+	    		scanf("%i", &y);
+	   			printf("\nKet Qua: %i - %i = %i ", x, y, x - y);
+	   			printf("\n    Thank you  ");
+	   			printf("******************** \n");
+	   			system("pause");
+	   			break;
+			case 3:
+				printf("Vui long nhap so X: ");
+	    		scanf("%i", &x);
+	    		printf("Vui long nhap so Y: ");
+	    		scanf("%i", &y);
+	   			printf("\nKet Qua: %i * %i = %i ", x, y, x * y);
+	   			printf("\n     Thank you");
+	   			printf("\n********************");
+	   			system("pause");
+	   			break;
+	   		case 4:
+	   			printf("Vui long nhap so X: ");
+	    		scanf("%i", &x);
+	    		printf("Vui long nhap so Y: ");
+	    		scanf("%i", &y);
+	   			printf("\nKet Qua: %i / %i = %i ", x, y, x / y);
+	   			printf("\n    Thank you ");
+	   			printf("\n********************");
+	   			system("pause");
+	   			break;				
+			}
+			if(z == 0){
+				printf("\n          ");
+				printf("Tam Biet Sir !");
+				break;
+			}
+			if(z != 1 && z != 2 && z != 3 && z != 4 && z != 0)
+			{
+				printf("Khong hop le. Vui long chon lai.");
+				system("pause");
+			}
+	}
+	return 0;
 }
