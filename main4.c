fa2a40b0640c058e6f9d6c9d0742d4329552a4b5
SlowV <viettqd00622@fpt.edu.vn>
2017-09-14 21:16:29 +0700

    test thu

---
+++ main4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void main(){
+	int i;
+	char arr[20]
+	for(i=0; i<=19; ++i){
+	
+	scanf(" %c", &arr[i]);
+	}
+	
+}
